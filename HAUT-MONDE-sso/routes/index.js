500851de3353382ff87ab5c19ae48ee0d6577490
Apu Sahasrabudhe <152632553+apusahas@users.noreply.github.com>
2023-12-18 11:38:04 -0500

    Add files via upload

--- HAUT-MONDE-sso/routes/index.js
+++ HAUT-MONDE-sso/routes/index.js
@@ -1,1 +1,97 @@
+import express from 'express'
+import session from 'express-session'
+import { WorkOS } from '@workos-inc/node'
 
+const app = express()
+const router = express.Router()
+
+app.use(
+    session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: true },
+    })
+)
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY);
+const clientID = process.env.WORKOS_CLIENT_ID;
+const organizationID = 'ENTER_YOUR_ORGID_HERE'
+const redirectURI = 'http://localhost:8000/callback'
+const state = ''
+
+router.get('/', function (req, res) {
+    if (session.isloggedin) {
+        res.render('login_successful.ejs', {
+            profile: session.profile,
+            first_name: session.first_name,
+            last_name: session.last_name,
+
+
+        })
+    } else {
+        res.render('index.ejs', { title: 'Home' })
+    }
+})
+
+router.post('/login', (req, res) => {
+    const login_type = req.body.login_method
+
+    const params = {
+        clientID: clientID,
+        redirectURI: redirectURI,
+        state: state,
+    }
+
+    if (login_type === 'saml') {
+        params.organization = organizationID
+    } else {
+        params.provider = login_type
+    }
+
+    try {
+        const url = workos.sso.getAuthorizationURL(params)
+
+        res.redirect(url)
+    } catch (error) {
+        res.render('error.ejs', { error: error })
+    }
+})
+
+router.get('/callback', async (req, res) => {
+    try {
+        const { code } = req.query
+
+        const profile = await workos.sso.getProfileAndToken({
+            code,
+            clientID,
+        })
+        const json_profile = JSON.stringify(profile, null, 4)
+
+        session.first_name = profile.profile.first_name
+        session.last_name = profile.profile.last_name
+        session.profile = json_profile
+        session.isloggedin = true
+
+        res.redirect('/')
+    } catch (error) {
+        res.render('error.ejs', { error: error })
+    }
+})
+
+
+router.get('/logout', async (req, res) => {
+    try {
+        session.first_name = null
+        session.profile = null
+        session.isloggedin = null
+
+        res.redirect('/')
+    } catch (error) {
+        res.render('error.ejs', { error: error })
+    }
+})
+
+
+
+export default router
